63d0d9011493ddb0644998a97560e7cd1edb51c3
dafsuy <jangkrikuburubur@gmail.com>
2024-09-07 12:58:22 +0700

    asdfsd

---
+++ src/app/components/common/MobileNav.tsx
@@ -0,0 +1,32 @@
+"use client";
+import clsx from "clsx";
+import { useState } from "react";
+export default function MobileNav() {
+  const [isOpenNav, setIsOpenNav] = useState<Boolean>(false);
+  return (
+    <header className="container fixed bg-primary py-4 flex flex-row justify-between items-center">
+      <span className="text-textDark font-bold">07TPLE004</span>
+      <div className="relative flex justify-center items-center">
+        <button
+          className={clsx(
+            "flex items-center justify-center w-8 h-8 p-2 rounded-full",
+            "z-30"
+          )}
+          onClick={() => setIsOpenNav(!isOpenNav)}
+        >
+          <span className={clsx("flex text-textDark items-center justify-center text-center align-middle", {'text-textWhite': isOpenNav})}>
+            {isOpenNav === false ? "..." : "X"}
+          </span>
+        </button>
+        <nav
+          className={clsx("bg-secondary shadow-2xl !z-20 transition-all", {
+            "w-full h-5/6 rounded-bl-full !z-20 duration-1000 opacity-100 fixed top-0 right-0":
+              isOpenNav,
+            "w-1 h-1 fixed duration-500 delay-200 -top-5 -right-5": !isOpenNav,
+          })}
+          onClick={() => setIsOpenNav(!isOpenNav)}
+        ></nav>
+      </div>
+    </header>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,100 +2,10 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+    <section className="pt-52 px-4">
+      <div className="container py-5 h-[200rem] !z-10 bg-secondary text-textWhite rounded-xl shadow-2xl">
+        P Lorem ipsum dolor, sit amet consectetur adipisicing elit. Numquam natus rerum molestiae consectetur ullam inventore blanditiis tempore perferendis, vel adipisci quis deleniti, repellendus, molestias illum voluptas modi temporibus quo. Sit aliquam autem iure natus tempore sed nobis modi eveniet asperiores, veritatis id deleniti nostrum numquam perferendis. Amet, iste laborum voluptatum maxime repellendus veniam consequuntur fuga, dolor molestias consequatur porro, odio quaerat quod quam ea placeat repellat vel iure. Unde officiis dolores ex magni atque quod, praesentium odit nihil repudiandae temporibus, excepturi vero hic ipsa soluta debitis consequuntur nisi nemo alias? Inventore non veniam expedita est distinctio repudiandae quibusdam eligendi sequi qui, hic nulla eius voluptatum sint alias ducimus nihil necessitatibus suscipit quae dolorum earum! Optio eius iure temporibus quae, accusantium minus distinctio eligendi, maiores, quod perferendis commodi libero. Delectus alias nulla corporis minus nesciunt dolorum magni pariatur voluptates? Minus hic dolorum fugiat libero dolore dolores necessitatibus excepturi ipsum, doloribus est blanditiis iste minima ullam id similique mollitia. Ipsam vitae aliquam cumque maiores quo iure suscipit, illo delectus. Laboriosam odit sit officia. Dolor unde quis libero quod autem est, ex, sed similique eligendi debitis quia expedita? Accusantium veritatis iusto ipsum esse doloribus, necessitatibus repellendus, fuga labore nulla debitis adipisci repudiandae sint.
+      </div>
+    </section>
   );
 }
